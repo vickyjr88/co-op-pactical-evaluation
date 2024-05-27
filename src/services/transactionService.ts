c9de18a7261554b07b853837b15a20215922e368
Victor Karanja <vickyjr888@gmail.com>
2024-05-27 11:42:03 +0300

    Add transaction logging functionality.

--- src/services/transactionService.ts
+++ src/services/transactionService.ts
@@ -1,5 +1,22 @@
 import db from "../config/database";
 import { credit } from "./accountService";
+import fs from "fs";
+import { createLogger, format, transports } from "winston";
+import path from "path";
+// Define the log file path
+const logFilePath = path.join(__dirname, "app.log");
+
+// Function to write logs to the file
+function logToFile(message: string) {
+  const timestamp = new Date().toISOString();
+  const logMessage = `${timestamp} - ${message}\n`;
+
+  fs.appendFile(logFilePath, logMessage, (err: any) => {
+    if (err) {
+      console.error("Failed to write to log file:", err);
+    }
+  });
+}
 
 const createTopUpTransaction = async (
   userId: string,
@@ -11,10 +28,12 @@ const createTopUpTransaction = async (
   db.run(
     `INSERT INTO transactions (userId, amount, category, type, date) VALUES (?, ?, ?, ?, ?)`,
     [userId, amount, category, type, date],
+
     function (err) {
       if (err) {
         console.error(err.message);
       } else {
+        logToFile(`Credited ${amount} to account ${userId}`);
         credit(amount, userId);
       }
     }
